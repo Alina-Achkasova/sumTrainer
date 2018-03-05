673463c79e6d90a4414b90e21cf3c635b3bac0d4
alina <alina@github.com>
2018-03-05 14:29:35 +0300

    styles changes

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 		var c=$("#coordinates")[0];
 		var ctx=c.getContext("2d");
 		ctx.strokeStyle = '#d45b91';
-		ctx.lineWidth = 2;
+		ctx.lineWidth = 2.5;
 		var coefficient = 38;
 		var bezierXforA = coefficient*firstNum + 1;
 		ctx.beginPath();
@@ -34,71 +34,75 @@ $(document).ready(function() {
 		$("#firstAnswer").fadeIn("slow");
 		$("#firstAnswer").focus();
 
-		$("#firstAnswer").on("input", function() {
-			if ($("#firstAnswer").val() === firstNum) {
-				$("#firstNumber").css('background-color', 'white');
-				$("#firstAnswer").css('background-color', 'white');
-				$("#firstAnswer").css('color', 'black');
-				$("#firstAnswer").css('border-color', 'transparent');
-				$("#firstAnswer").val(firstNum);
-				$("#firstAnswer").prop('disabled', true);
-				
-				var bezierXforB = coefficient*secondNum + 1 + bezierXforA;
-				ctx.beginPath();
-				ctx.moveTo(bezierXforA, 60);
-				ctx.bezierCurveTo(bezierXforA, 0, bezierXforB, 0, bezierXforB, 60);
-				ctx.moveTo(bezierXforB, 60);
-			    ctx.lineTo(bezierXforB - 8, 50);
-			    ctx.moveTo(bezierXforB, 60);
-			    ctx.lineTo(bezierXforB + 4, 50);
-				ctx.stroke();
-				$("#secondAnswer").show();
-				$("#secondAnswer").css('margin-left', bezierXforB/2 - 25 + 'px');
-				$("#secondAnswer").focus();
+		$("#firstAnswer").bind('blur keyup',function(e) {
+			if (e.type === 'blur' || e.keyCode === 13) {
+				if ($("#firstAnswer").val() === firstNum) {
+					$("#firstNumber").css('background-color', 'white');
+					$("#firstAnswer").css('background-color', 'white');
+					$("#firstAnswer").css('color', 'black');
+					$("#firstAnswer").css('border-color', 'transparent');
+					$("#firstAnswer").val(firstNum);
+					$("#firstAnswer").prop('disabled', true);
+					
+					var bezierXforB = coefficient*secondNum + 1 + bezierXforA;
+					ctx.beginPath();
+					ctx.moveTo(bezierXforA, 60);
+					ctx.bezierCurveTo(bezierXforA, 0, bezierXforB, 0, bezierXforB, 60);
+					ctx.moveTo(bezierXforB, 60);
+				    ctx.lineTo(bezierXforB - 8, 50);
+				    ctx.moveTo(bezierXforB, 60);
+				    ctx.lineTo(bezierXforB + 4, 50);
+					ctx.stroke();
+					$("#secondAnswer").show();
+					$("#secondAnswer").css('margin-left', bezierXforB/2 - 25 + 'px');
+					$("#secondAnswer").focus();
 
-				$("#secondAnswer").on("input", function() {
-					if ($("#secondAnswer").val() === secondNum) {
-						$("#secondNumber").css('background-color', 'white');
-						$("#secondAnswer").css('border-color', 'transparent');
-						$("#secondAnswer").css('color', 'black');
-						$("#secondAnswer").val(secondNum);
-						$("#secondAnswer").css('background-color', 'white');
-						$("#secondAnswer").prop('disabled', true);
-						$("#resultInput").val('');
-						$("#resultInput").css('border', '1px solid #999');
-						$("#resultInput").focus();
+					$("#secondAnswer").bind('blur keyup',function(e) {
+						if (e.type === 'blur' || e.keyCode === 13) {
+							if ($("#secondAnswer").val() === secondNum) {
+								$("#secondNumber").css('background-color', 'white');
+								$("#secondAnswer").css('border-color', 'transparent');
+								$("#secondAnswer").css('color', 'black');
+								$("#secondAnswer").val(secondNum);
+								$("#secondAnswer").css('background-color', 'white');
+								$("#secondAnswer").prop('disabled', true);
+								$("#resultInput").val('');
+								$("#resultInput").css('border', '1px solid #999');
+								$("#resultInput").focus();
 
-						$("#resultInput").on("input", function() {
-						    $("#resultInput").css('color', 'black');
-						    $("#resultInput").bind('blur keyup',function(e) {
-								if (e.type === 'blur' || e.keyCode === 13) {
-									$("#resultInput").css('color', 'black');
-									if ($("#resultInput").val() == +firstNum + +secondNum) {
-										$("#resultInput").css('border-color', 'transparent');
-										$("#resultInput").val(+firstNum + +secondNum);
-										$("#resultInput").css('background-color', 'white');
-										$("#resultInput").prop('disabled', true);
-										$("#newTask").css("opacity", 1);
-										$("#newTask").click(function() {
-											localStorage.setItem("firstTime", "false");
-											location.reload();
-										});
+								$("#resultInput").on("input", function() {
+								    $("#resultInput").css('color', 'black');
+								    $("#resultInput").bind('blur keyup',function(e) {
+										if (e.type === 'blur' || e.keyCode === 13) {
+											$("#resultInput").css('color', 'black');
+											if ($("#resultInput").val() == +firstNum + +secondNum) {
+												$("#resultInput").css('border-color', 'transparent');
+												$("#resultInput").val(+firstNum + +secondNum);
+												$("#resultInput").css('background-color', 'white');
+												$("#resultInput").prop('disabled', true);
+												$("#newTask").css("opacity", 1);
+												$("#newTask").click(function() {
+													localStorage.setItem("firstTime", "false");
+													location.reload();
+												});
 
-									} else {
-										$("#resultInput").css('color', 'red');
-									}
-								}
-							});
-						});
-						
-					} else {
-						$("#secondAnswer").css('color', 'red');
-						$("#secondNumber").css('background-color', '#faa84b');
-					}
-				});
-			} else {
-				$("#firstAnswer").css('color', 'red');
-				$("#firstNumber").css('background-color', '#faa84b');
+											} else {
+												$("#resultInput").css('color', 'red');
+											}
+										}
+									});
+								});
+								
+							} else {
+								$("#secondAnswer").css('color', 'red');
+								$("#secondNumber").css('background-color', '#faa84b');
+							}
+						}
+					});
+				} else {
+					$("#firstAnswer").css('color', 'red');
+					$("#firstNumber").css('background-color', '#faa84b');
+				}
 			}
 		})	
 	});
